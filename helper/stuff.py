d845a1d63f7a599659e37a9aa89cd242f656d20b
kucbkto <95414625+kucbkto@users.noreply.github.com>
2021-12-24 17:23:54 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -29,12 +29,12 @@ async def up(event):
 async def start(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"Hi `{ok.user.first_name}`\nThis is A CompressorBot Which Can Encode Videos.\nReduce Size of Videos With Negligible Quality Change\nU can Generate Samples/screenshots too.",
+        f"Hi `{ok.user.first_name}`\nKAISE HAI AAPLOG.\nhttps://t.me/+lSVK6MINpzVkNzVl\nJOIN KRNA.",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SHARE ME", url="https://t.me/share/url?url=t.me/Demo_compressbot"),
+                Button.url("DEVELOPER", url="t.me/misterstudant"),
             ],
         ],
     )
